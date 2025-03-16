ca5de1f4d9a24fdda88565713f991796be477c52
jonghoon <jonghoon.1738@gmail.com>
2025-03-16 12:20:08 +0900

    test code

---
+++ Test/IService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test;
+public interface IServiceA { void Execute(); }
+public class ServiceA : IServiceA { public void Execute() => Console.WriteLine("ServiceA Executed"); }
+
+public interface IServiceB { string GetData(); }
+public class ServiceB : IServiceB { public string GetData() => "ServiceB Data"; }
+
+public interface IServiceC { int Calculate(int x, int y); }
+public class ServiceC : IServiceC { public int Calculate(int x, int y) => x + y; }
+
+public interface IServiceD { void Process(string input); }
+public class ServiceD : IServiceD { public void Process(string input) => Console.WriteLine($"Processed: {input}"); }
+
+public interface IServiceE { DateTime GetCurrentTime(); }
+public class ServiceE : IServiceE { public DateTime GetCurrentTime() => DateTime.UtcNow; }
+
+public interface IServiceF { bool Validate(string input); }
+public class ServiceF : IServiceF { public bool Validate(string input) => !string.IsNullOrEmpty(input); }
+
+public interface IServiceG { void LogMessage(string message); }
+public class ServiceG : IServiceG { public void LogMessage(string message) => Console.WriteLine($"Log: {message}"); }
+
+public interface IServiceH { double Compute(double a, double b); }
+public class ServiceH : IServiceH { public double Compute(double a, double b) => a * b; }
+
+public interface IServiceI { Guid GenerateId(); }
+public class ServiceI : IServiceI { public Guid GenerateId() => Guid.NewGuid(); }
+
+public interface IServiceJ { string FormatText(string text); }
+public class ServiceJ : IServiceJ { public string FormatText(string text) => text.ToUpper(); }
---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using ServiceCollectionExporter;
+using Test;
+
+var services = new ServiceCollection();
+
+// 다양한 Lifetime 지정
+services.AddTransient<IServiceA, ServiceA>();
+services.AddScoped<IServiceB, ServiceB>();
+services.AddSingleton<IServiceC, ServiceC>();
+services.AddTransient<IServiceD, ServiceD>();
+services.AddScoped<IServiceE, ServiceE>();
+services.AddSingleton<IServiceF, ServiceF>();
+services.AddTransient<IServiceG, ServiceG>();
+services.AddScoped<IServiceH, ServiceH>();
+services.AddSingleton<IServiceI, ServiceI>();
+services.AddTransient<IServiceJ, ServiceJ>();
+
+// JSON으로 내보내기
+services.ExportToJson();
